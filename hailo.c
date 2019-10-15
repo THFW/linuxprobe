41e26673020c936d290eab00b3a053084ca73841
Yang Chen Long <820371826@qq.com>
2019-10-15 17:29:25 +0800

    add hailo.c

--- hailo.c
+++ hailo.c
@@ -1,3 +1,49 @@
+#include<stdio.h>
+#define N 10
+int main()
+{
+    int a[N],b,j,t,i,n,m;
+	n=1;
+	for(i=0;i<N;i++)
+    scanf("%d",a+i);
+	b=a[N-1];
+	for(j=0;j<N;j++)
+	{
+		if((b>a[j-1]&&b<a[j])||(b>a[j]&&b<a[j-1]))
+		{
+			t=j;
+			m=a[j];
+			a[j]=b;
+			for(i=9;i>t+1;i--)
+			{
+				a[i]=a[i-1];
+			}
+			a[t+1]=m;
+		}	
+	}
+	for(b=0;b<10;b++)
+	printf("%d\n",a[b]);
+	
+	
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if 0
 #include <stdio.h>
 #include <stdlib.h>
 struct tong{
@@ -12,9 +58,8 @@ void output_file(struct tong  q[],int n);
 
 int main()
 {
-	
-	struct tong t[n];
 	int n,i;
+	struct tong t[n];
 	scanf("%d",&n);
 	
 	input_file(t,n);
@@ -46,27 +91,7 @@ void output_file(struct tong q[], int n)
 	}
 	}
 }
-
-
-#if 0
-#include<stdio.h>
-#define N 10
-int main()
-{
-    int a[N],b,j,t,i;
-	for(i=0;i<N;i++)
-    scanf("%d",a+i);
-	b=a[N-1];
-	for(j=0;j<N;j++)
-	{
-		if(b>a[j])
-		{
-			t=a[j+1];
-			a[j+1]=a[j];
-		}
-	}
-	for(j=0;j<N;j++)
-	printf("%d\n",a[j]);
-    return 0;
-}
 #endif
+
+
+
