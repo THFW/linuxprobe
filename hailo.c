81daebbf6e1883dad1ddb7bdf5cdf823290d83d8
Yang Chen Long <820371826@qq.com>
2019-10-23 23:49:00 +0800

    add hailo.c

--- hailo.c
+++ hailo.c
@@ -1,37 +1,62 @@
 #include<stdio.h>
-#define N 100
-char input_file(char a[N]);
-int i=0,b=0,c=0,d=0,e=0;
+int input_file(int a[10]);
+int deal_file(int a[10]);
+int output_file(int a[10]);
 int main()
 {
-	char t[N];
-	gets(t);
+    int t[10];
+
 	input_file(t);
-	printf("%d %d %d %d",c,b,d,e);
+	deal_file(t);
+	output_file(t);
     return 0;
 }
 
-char input_file(char a[N])
-{
-    
-	
-   // while(~scanf("%d%d", &a, &b))printf("%d\n",a+b);
-	
-	for(i=0;i<N;i++)
+
+int input_file(int a[10])
+{	int i;
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",a+i);
+	}
+	return 0;
+}
+
+int deal_file(int a[10])
+{	
+	int b[10],i,j,t=0,tmp=0;
+	for(i=0;i<10;i++)
+	{
+		b[i]=i;
+	}
+    for(i=0;i<10;i++)
 	{	
-	
-		if((a[i] >= '0')&&(a[i]<='9'))
-			b++;
-		else if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
-			c++;
-		else if(a[i]==' ')
-			d++;
-		else
+		for(j=i+1;j<10;j++)
 		{
-			if(a[i]=='\0') break;
-			e++;
+			if(a[b[i]]>a[b[j]])
+			{
+				t=b[i];
+				b[i]=b[j];
+				b[j]=t;
+			}
 		}
 	}
+
+	t=a[b[0]];
+	a[b[0]]=a[0];
+	a[0]=t;
+
+	tmp=a[b[9]];
+	a[b[9]]=a[9];
+	a[9]=tmp;
+
+	return 0;
+}
+
+int output_file(int a[10])
+{	int i;
+	for(i=0;i<10;i++)
+		printf("%d ",a[i]);
 
 	return 0;
 }
