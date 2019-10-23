816b850f39525cbe97eb0ff9658e2cc365d9fa08
Yang Chen Long <820371826@qq.com>
2019-10-23 12:33:04 +0800

    add hailo.c

--- hailo.c
+++ hailo.c
@@ -1,13 +1,115 @@
+#if 1
 #include<stdio.h>
+#define N  10
+struct tong
+{
+	char name[10];
+	unsigned  long  birthday;
+	char num[17];
+};
+void input_file(struct tong q[], int n);
+void output_file(struct tong p[],int n);
+
 int main()
 {
-    int a,b,c,d,e;
-	scanf("%d",&a);
-	b=a%10;
-	c=(a/10)%10;
-	d=a/100%10;
-	e=a/1000%10;
-	printf("%d %d %d %d",e,d,c,b);
+	int n;
+    struct tong t[N];
+	scanf("%d",&n);
+	input_file(t,n);
+	output_file(t,n);
 
     return 0;
 }
+void input_file(struct tong q[], int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+		scanf("%s %lu %s",q[i].name,&q[i].birthday,q[i].num);
+}
+void output_file(struct tong p[],int n)
+{
+	int num,i,j, t;
+/*	char *a,*b,*tmp;
+	char *c,*d,*e;
+	tmp=char malloc(10*sizeof(char));
+	e=char malloc(17*sizeof(char));
+	for(i=0;i<10;i++)
+	{
+	
+		for(j=i+1;j<10;j++)
+		{
+			if(p[i].birthday>p[j].birthday)
+			{
+				t=p[i].birthday;
+				p[i].birthday=p[j].birthday;
+				p[j].birthday=t;
+
+				a=p[i].name;
+				b=p[j].name;
+				*tmp=*a;
+				*a=*b;
+				*b=*tmp;
+
+				c=p[i].num;
+				d=p[j].num;
+
+				*e=*c;
+				*c=*d;
+				*d=*e;
+
+			}
+		}
+	}
+
+*/
+
+	for(i=0;i<10;i++)
+	{
+	
+		for(j=i+1;j<10;j++)
+		{
+			if(p[i].birthday>p[j].birthday)
+			{
+				t=p[i].birthday;
+				p[i].birthday=p[j].birthday;
+				p[j].birthday=t;
+			}
+		}
+	}
+	for(j=0;j<n;j++)
+	{
+		printf("%s %lu %s \n",p[j].name,p[j].birthday,p[j].num);
+	}
+
+}
+
+#else
+
+#include<stdio.h>
+int main()
+{
+    int b,i,j,t=0;
+	int a[10]={2 ,5 ,4 ,13 ,1 ,8 ,34 ,11,6,3};
+    for(i=0;i<10;i++)
+	{
+	
+		for(j=i+1;j<10;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	for(i=0;i<10;i++)
+	{
+		printf("%d   ",a[i]);
+	}
+
+	b=19850403;
+	printf("%d",b);
+    return 0;
+}
+#endif
