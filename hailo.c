9d49deb8a482464cc4c768a4ba196cb9a9cec9f7
Yang Chen Long <820371826@qq.com>
2019-10-24 22:29:57 +0800

    add hailo.c

--- hailo.c
+++ hailo.c
@@ -1,62 +1,50 @@
 #include<stdio.h>
-int input_file(int a[10]);
-int deal_file(int a[10]);
-int output_file(int a[10]);
-int main()
-{
-    int t[10];
+#define N 10
 
-	input_file(t);
+void deal_file(int a[N])
+{
+	int i,t,b,max,min;
+	max=min=a[0];
+		
+	for(i=0;i<10;i++)
+	{
+		if(max<a[i])
+		{
+			max=a[i];
+			b=i;
+		}
+	}
+	t=a[9];a[9]=a[b];a[b]=t;
+	for(i=0;i<10;i++)
+	{
+		if(min>a[i])
+		{
+			min=a[i];
+			b=i;
+		}
+	}
+	t=a[0];a[0]=a[b];a[b]=t;
+
+}
+
+void output_file(int a[N])
+{	
+	int i;
+	for(i=0;i<10;i++)
+		printf("%d ",a[i]);
+	printf("\n");
+}
+
+int main(void)
+{
+    int t[N];
+	int i;
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&t[i]);
+	}
 	deal_file(t);
 	output_file(t);
     return 0;
 }
-
-
-int input_file(int a[10])
-{	int i;
-	for(i=0;i<10;i++)
-	{
-		scanf("%d",a+i);
-	}
-	return 0;
-}
-
-int deal_file(int a[10])
-{	
-	int b[10],i,j,t=0,tmp=0;
-	for(i=0;i<10;i++)
-	{
-		b[i]=i;
-	}
-    for(i=0;i<10;i++)
-	{	
-		for(j=i+1;j<10;j++)
-		{
-			if(a[b[i]]>a[b[j]])
-			{
-				t=b[i];
-				b[i]=b[j];
-				b[j]=t;
-			}
-		}
-	}
-
-	t=a[b[0]];
-	a[b[0]]=a[0];
-	a[0]=t;
-
-	tmp=a[b[9]];
-	a[b[9]]=a[9];
-	a[9]=tmp;
-
-	return 0;
-}
-
-int output_file(int a[10])
-{	int i;
-	for(i=0;i<10;i++)
-		printf("%d ",a[i]);
-
-	return 0;
-}
+//This is correct answer others are error
